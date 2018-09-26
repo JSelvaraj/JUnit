ce999fa145e71f6af2f30f7d99f83f37e88b363c
JSelvaraj <selvarajjeffrey@yahoo.co.uk>
2018-09-26 16:36:26 +0100

    Add files via upload

---
+++ W03-JUnit/code/src/impl/LoyaltyCardOwner.java
@@ -0,0 +1,29 @@
+package impl;
+
+import interfaces.ILoyaltyCardOwner;
+
+/**
+ * This class represents loyalty card owners.
+ *
+ */
+public class LoyaltyCardOwner implements ILoyaltyCardOwner {
+
+    public String email;
+    public String name;
+
+    public LoyaltyCardOwner (String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+}
